83e733a8d23b0dffa5623b3e6b3247de4a04376a
xuechuan mi <xuechuanmi@xuechuans-macbook-pro.local>
2018-05-19 19:49:49 -0400

    just try

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mi
+//
+//  Created by xuechuan mi on 2018-05-19.
+//  Copyright © 2018 xuechuan mi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
